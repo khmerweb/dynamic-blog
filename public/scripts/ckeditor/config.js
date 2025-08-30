909485f5726a6dec6e8d71010b7f41f50893883a
khmerweb <vuthdevelop1@gmail.com>
2025-08-30 16:04:46 +0700

    Make it better

--- public/scripts/ckeditor/config.js
+++ public/scripts/ckeditor/config.js
@@ -1,30 +1,30 @@
+// public/scripts/ckeditor/config.js
 var ckeditor
 
 ClassicEditor
-        .create( document.querySelector( '#editor' ), {
-          
-          toolbar: ['fontfamily', 'fontsize', 'fontcolor', 'bold', 'italic', 
-          'bulletedList', 'indent', 'outdent', 'numberedList', 'link', 'blockQuote', 
-          'insertTable','code', 'codeblock', 'imageinsert', 'mediaembed', 'undo', 'redo' ],
-          fontFamily: {
+    .create( document.querySelector( '#editor' ), {
+        toolbar: ['fontfamily', 'fontsize', 'fontcolor', 'bold', 'italic', 
+            'bulletedList', 'indent', 'outdent', 'numberedList', 'link', 'blockQuote', 
+            'insertTable','code', 'codeblock', 'imageinsert', 'mediaembed', 'undo', 'redo' ],
+        fontFamily: {
             options: [
-              'ឧត្តមាន​ជ័យ, OdorMeanChey', 'អក្សរដៃ, HandWriting',
-              'គូលេន, Koulen', 'ក្រូច​ឆ្នារ, Limonf3',
-              'បាយ័ន, Bayon', 'ក្រសាំង, Rooster',
-              'មូល, Moul',
-    				  'Arial, Helvetica, sans-serif',
- 				      'Courier New, Courier, monospace',
- 				      'Georgia, serif',
- 				      'Lucida Sans Unicode, Lucida Grande, sans-serif',
- 				      'Tahoma, Geneva, sans-serif',
- 				      'Times New Roman, Times, serif',
- 				      'Trebuchet MS, Helvetica, sans-serif',
-				      'Verdana, Geneva, sans-serif',
+                'ឧត្តមាន​ជ័យ, OdorMeanChey', 'អក្សរដៃ, HandWriting',
+                'គូលេន, Koulen', 'ក្រូច​ឆ្នារ, Limonf3',
+                'បាយ័ន, Bayon', 'ក្រសាំង, Rooster',
+                'មូល, Moul',
+    				    'Arial, Helvetica, sans-serif',
+ 				        'Courier New, Courier, monospace',
+ 				        'Georgia, serif',
+ 				        'Lucida Sans Unicode, Lucida Grande, sans-serif',
+ 				        'Tahoma, Geneva, sans-serif',
+ 				        'Times New Roman, Times, serif',
+ 				        'Trebuchet MS, Helvetica, sans-serif',
+				        'Verdana, Geneva, sans-serif',
             ],
             supportAllValues: true
-          },
+        },
           
-          fontSize: {
+        fontSize: {
             options: [
                 9,
                 11,
@@ -35,10 +35,10 @@ ClassicEditor
                 21
             ],
             supportAllValues: true
-          },
+        },
 
-          codeBlock: {
-			        languages: [
+        codeBlock: {
+			      languages: [
 				      // Do not render the CSS class for the plain text code blocks.
 				      { language: 'plaintext', label: 'Plain text', class: '' },
               { language: 'svelte', label: 'Svelte', class: 'svelte' },
@@ -54,15 +54,15 @@ ClassicEditor
 				      // Python code blocks will have the default "language-python" CSS class.
 				      { language: 'python', label: 'Python' }
             ] 
-          },
-        })
-        .then( editor => {
+        },
+      })
+      .then( editor => {
           ckeditor = editor
           editor.editing.view.focus()
           editor.editing.view.change(writer=>{
-              writer.setStyle('height', '260px', editor.editing.view.document.getRoot())
+              writer.setStyle('height', '270px', editor.editing.view.document.getRoot())
           })
-        })
-        .catch( err => {
+      })
+      .catch( err => {
           console.error( err.stack )
-        });
+      })
--- src/components/admin/Item.svelte
+++ src/components/admin/Item.svelte
@@ -1,1 +1,140 @@
-<p>Items</p>
+<!--src/components/admin/Item.svelte-->
+<script>
+    let {data} = $props()
+	let items = $state(data?.items)
+	let value = $state(undefined)
+	let navPage = data?.navPage || 1
+	
+	async function paginate(e){
+		const response = await fetch(`/admin/${data.type}/paginate/${e.target.value}`)
+		items = await response.json()
+	}
+</script>
+
+<footer>
+    <div class="info">{data?.info}​ទាំងអស់​មានចំនួនៈ {data?.count}</div>
+    <div class="items">
+        {#each items as item}
+        <div class="item">
+            <a class="thumb" href="/{data.type}/{item.id}">
+                <img src={item.thumb} alt='' />
+                {#if item.videos?.length}
+                <img class="play" src="/images/play.png" alt='' />
+                {/if}
+            </a>
+            <div class="title">
+                <a href="/{data.type}/{item.id}">{item.title}</a>
+                <div>{new Date(item.date).toLocaleDateString('it-IT')}</div>
+            </div>
+            <div class="edit">
+				<a href="/admin/{data.type}/delete/{item.id}">
+					<img src="/images/delete.png" alt=''/>
+				</a>
+                <a style="padding-right:5px;" href={`/admin/${data.type}/edit/${item.id}?p=${value}`}>
+					<img src="/images/edit.png" alt='' />
+				</a>
+            </div> 
+        </div>
+        {/each}
+    </div>
+	<div class="pagination">
+		<span>​​​​​​​​​​​​​​​​​​​​​ទំព័រ </span>
+			<select bind:value onchange={paginate}> 
+				{#each [...Array(data?.pageNumber).keys()] as page}
+					{#if page+1 == parseInt(navPage)}
+					<option selected>{page+1}</option>
+					{:else}
+					<option>{page+1}</option>
+					{/if}
+				{/each}
+			</select> 
+            
+        <span>នៃ {data?.pageNumber}</span>
+	</div>
+</footer>
+
+<style>
+    footer{
+		margin-top: 0;
+	}
+	footer .info{
+		background-color: var(--background);
+		text-align: center;
+		margin-top: 10px;
+		padding: 5px;
+	}
+	footer .items{
+		display: grid;
+		grid-template-columns: calc(50% - 5px ) calc(50% - 5px );
+		grid-gap: 10px;
+		margin-top: 10px;
+	}
+	footer .items .item{
+		background-color: var(--background);
+		display: grid;
+		grid-template-columns: 130px auto;
+		grid-gap: 10px;
+		align-items: center;
+		padding-right: 10px;
+	}
+	footer .items .item:hover{
+		grid-template-columns: 130px auto 75px;
+	}
+	footer .items .item .thumb{
+		position: relative;
+		padding-top: 56.25%;
+		overflow: hidden;
+	}
+	footer .items .item .thumb img{
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		min-height: 100%;
+		float: left;
+	}
+	footer .items .item .thumb .play{
+		width: 25%;
+		top: 50%;
+    	left: 50%;
+		min-height: auto;
+		transform: translate(-50%,-50%);
+	}
+	footer .items .item .title{
+		white-space: nowrap;
+        overflow: hidden;
+    	text-overflow: ellipsis;
+		
+	}
+	footer .items .item .edit{
+    	text-align: right;
+		display: none;
+	}
+	footer .items .item .edit a{
+    	float: right;
+	}
+	footer .items .item .edit img{
+		width: 30px;
+	}
+	footer .items .item .edit img:hover{
+		cursor: pointer;
+		opacity: .7;
+	}
+	footer .items .item:hover .edit{
+    	display: block;
+		grid-template-columns: auto auto;
+	}
+	footer .pagination{
+		text-align: center;
+		margin-top: 20px;
+	}
+
+    @media only screen and (max-width: 600px){
+        footer .items{
+            grid-template-columns: 100%;
+        }
+        footer .items .item .edit img{
+		    width: 35px;
+	    }
+    }
+</style>
